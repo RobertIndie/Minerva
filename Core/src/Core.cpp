862b12ace369c5110db0f0ae34fb8e5f8c31c3d8
AaronRobert <Robert.Aaron2017@outlook.com>
2018-06-13 15:41:21 +0800

    first window

--- Core/src/Core.cpp
+++ Core/src/Core.cpp
@@ -1,13 +1,49 @@
-﻿// Core.cpp : Defines the entry point for the application.
-//
-
-#include "Core.h"
+﻿#include "Core.h"
 
 using namespace std;
 
+void ErrorCallback(int error, const char* description)
+{
+	cout << "[ERROR]" << description << endl;
+}
+
+void WindowCloseCallback(GLFWwindow* window)
+{
+
+}
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
 int main()
 {
-	glfwInit();
-	cout << "Hello CMake." << endl;
+	if (!glfwInit())
+	{
+		cout << "Init failed" << endl;
+	}
+	glfwSetErrorCallback(ErrorCallback);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Test window", NULL, NULL);
+	if (!window)
+	{
+		cout << "Window creation failed" << endl;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetWindowCloseCallback(window, WindowCloseCallback);
+	glfwSetKeyCallback(window, KeyCallback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
